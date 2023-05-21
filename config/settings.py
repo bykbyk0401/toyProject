54c39959a87c7319785f37dd479673b32adcdc26
bykbyk0401 <bykbyk0401@gmail.com>
2023-05-21 23:09:57 +0900

    수정

--- config/settings.py
+++ config/settings.py
@@ -70,6 +70,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
+    "185.199.111.153:80",
     #"http://toyproject-gyb-byk.kro.kr/",
 ]
 
